14e4508784efb9725448353a023f16ec80375e52
ramtinmk <ramtinmk1382@gmail.com>
2025-12-18 14:19:24 +0330

    added table class

---
+++ include/Table.h
@@ -0,0 +1,42 @@
+#include "Row.h"
+
+
+
+
+
+
+
+class Table{
+
+    std::vector<Row> rows;
+
+    public:
+        void insert_row(Row& row){
+
+            rows.push_back(row);
+
+
+        }
+
+        Row& get_row(uint32_t index){
+
+            return rows[index];
+        }
+
+
+
+        size_t size(){
+
+            return rows.size();
+        }
+
+        void print_table(){
+            for (uint32_t i=0;i<size();i++){
+                rows[i].print();
+            }
+        }
+
+        void flush_to_disk();
+
+        void open_db_files();
+}
